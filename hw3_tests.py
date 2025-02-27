7b4b6a8021393871a5e4ce0a368320a36ed78081
dk545 <dkeshav@calpoly.edu>
2025-02-26 22:20:03 -0800

    submission commit

--- hw3_tests.py
+++ hw3_tests.py
@@ -2,6 +2,7 @@ import data
 import build_data
 import unittest
 
+import hw3
 
 # These two values are defined to support testing below. The
 # data within these structures should not be modified. Doing
@@ -175,32 +176,191 @@ reduced_data = [
         'WY')
     ]
 
+sample = data.CountyDemographics(
+        # age
+        {'Percent 65 and Older': 17.5,
+         'Percent Under 18 Years': 18.1,
+         'Percent Under 5 Years': 4.8},
+        # county
+        'San Luis Obispo County',
+        # education
+        {"Bachelor's Degree or Higher": 31.5,
+         'High School or Higher': 89.6},
+        # ethnicities
+        {'American Indian and Alaska Native Alone': 1.4,
+         'Asian Alone': 3.8,
+         'Black Alone': 2.2,
+         'Hispanic or Latino': 22.0,
+         'Native Hawaiian and Other Pacific Islander Alone': 0.2,
+         'Two or More Races': 3.4,
+         'White Alone': 89.0,
+         'White Alone, not Hispanic or Latino': 69.5},
+        # income
+        {'Median Household Income': 58697,
+         'Per Capita Income': 29954,
+         'Persons Below Poverty Level': 14.3},
+        # population
+        {'2010 Population': 269637,
+         '2014 Population': 279083,
+         'Population Percent Change': 3.5,
+         'Population per Square Mile': 81.7},
+        # state
+        'CA'
+    )
+
 class TestCases(unittest.TestCase):
     pass
 
     # Part 1
     # test population_total
-
+    def test_population_total(self):
+        self.assertEqual(hw3.population_total(build_data.get_data()),318857056)
+    def test_population_total_2(self):
+        expected = hw3.population_total(reduced_data)
+        actual = 0
+        for i in reduced_data:
+            actual += i.population['2014 Population']
+        self.assertEqual(expected,actual)
     # Part 2
     # test filter_by_state
-
+    def test_filter_by_state(self):
+        self.assertEqual(hw3.filter_by_state(reduced_data,'AL'),[reduced_data[0]])
+    def test_filter_by_state_2(self):
+        self.assertEqual(hw3.filter_by_state(reduced_data,'NY'),[])
     # Part 3
     # test population_by_education
+    def test_population_by_education(self):
+        self.assertEqual(hw3.population_by_education([sample],"Bachelor's Degree or Higher"),87911.145)
+    def test_population_by_education_2(self):
+        actual = 0
+        for i in reduced_data:
+            actual+=i.education['High School or Higher']*i.population['2014 Population']/100
+        expected = hw3.population_by_education(reduced_data,'High School or Higher')
+        self.assertEqual(expected,actual)
     # test population_by_ethnicity
+    def test_population_by_ethnicity(self):
+        self.assertEqual(hw3.population_by_ethnicity([sample],'Two or More Races'),9488.822)
+    def test_population_by_ethnicity_2(self):
+        actual = 0
+        for i in reduced_data:
+            actual += i.ethnicities['Black Alone'] * i.population['2014 Population'] / 100
+        expected = hw3.population_by_ethnicity(reduced_data, 'Black Alone')
+        self.assertEqual(expected, actual)
     # test population_below_poverty_level
-
+    def test_population_below_poverty(self):
+        expected = hw3.population_below_poverty_level([sample])
+        actual = sample.income['Persons Below Poverty Level'] * sample.population['2014 Population']/100
+        self.assertEqual(expected,actual)
+    def test_population_below_poverty_2(self):
+        expected = hw3.population_below_poverty_level([sample])
+        actual = sample.income['Per Capita Income'] * sample.population['2014 Population'] / 100
+        self.assertNotEqual(expected, actual)
     # Part 4
     # test percent_by_education
+    def test_percent_by_education(self):
+        subpopulation = (reduced_data[0].education['High School or Higher'] * reduced_data[0].population['2014 Population']) / 100
+        expected = (subpopulation / reduced_data[0].population['2014 Population']) * 100
+        actual = hw3.percent_by_education([reduced_data[0]], 'High School or Higher')
+        self.assertEqual(expected, actual)
+    def test_percent_by_education_2(self):
+        subpopulation = (reduced_data[0].education["Bachelor's Degree or Higher"] * reduced_data[0].population['2014 Population']) / 100
+        expected = (subpopulation / reduced_data[0].population['2014 Population']) * 100
+        actual = hw3.percent_by_education([reduced_data[0]], "Bachelor's Degree or Higher")
+        self.assertEqual(expected, actual)
     # test percent_by_ethnicity
+    def test_percent_by_ethnicity(self):
+        subpopulation = (reduced_data[0].ethnicities['Asian Alone'] * reduced_data[0].population['2014 Population']) / 100
+        expected = (subpopulation / reduced_data[0].population['2014 Population']) * 100
+        actual = hw3.percent_by_ethnicity([reduced_data[0]], 'Asian Alone')
+        self.assertEqual(expected, actual)
+    def test_percent_by_ethnicity_2(self):
+        subpopulation = (reduced_data[0].ethnicities['Black Alone'] * reduced_data[0].population['2014 Population']) / 100
+        expected = (subpopulation / reduced_data[0].population['2014 Population']) * 100
+        actual = hw3.percent_by_ethnicity([reduced_data[0]], 'Black Alone')
+        self.assertEqual(expected, actual)
     # test percent_below_poverty_level
+    def test_percent_below_poverty_level(self):
+        subpopulation = (reduced_data[0].income['Persons Below Poverty Level']* reduced_data[0].population['2014 Population']) / 100
+        expected = (subpopulation/reduced_data[0].population['2014 Population'])*100
+        actual = hw3.percent_below_poverty_level([reduced_data[0]])
+        self.assertEqual(expected,actual)
+    def test_percent_below_poverty_level_2(self):
+        subpopulation = (reduced_data[0].income['Median Household Income']* reduced_data[0].population['2014 Population']) / 100
+        expected = (subpopulation/reduced_data[0].population['2014 Population'])*100
+        actual = hw3.percent_below_poverty_level([reduced_data[0]])
+        self.assertNotEqual(expected,actual)
 
     # Part 5
     # test education_greater_than
+    def test_education_greater_than(self):
+        percentage = 90
+        expected = [reduced_data[6]]
+        actual = hw3.education_greater_than(reduced_data,'High School or Higher',percentage)
+        self.assertEqual(expected,actual)
+    def test_education_greater_than_2(self):
+        percentage = 30
+        expected = [reduced_data[2],reduced_data[3]]
+        actual = hw3.education_greater_than(reduced_data, "Bachelor's Degree or Higher", percentage)
+        self.assertEqual(expected, actual)
     # test education_less_than
+    def test_education_less_than(self):
+        percentage = 90
+        expected = [reduced_data[0],reduced_data[1],reduced_data[2],reduced_data[3],reduced_data[4],reduced_data[5]]
+        actual = hw3.education_less_than(reduced_data,'High School or Higher',percentage)
+        self.assertEqual(expected,actual)
+    def test_education_less_than_2(self):
+        percentage = 40
+        expected = [reduced_data[0],reduced_data[1],reduced_data[2],reduced_data[3],reduced_data[4],reduced_data[5],reduced_data[6]]
+        actual = hw3.education_less_than(reduced_data,"Bachelor's Degree or Higher",percentage)
+        self.assertEqual(expected,actual)
     # test ethnicity_greater_than
+    def test_ethnicity_greater_than(self):
+        expected = [reduced_data[3]]
+        actual = hw3.ethnicity_greater_than(reduced_data,'Asian Alone',5)
+        self.assertEqual(expected,actual)
+    def test_ethnicity_greater_than_2(self):
+        expected = [reduced_data[0]]
+        actual = hw3.ethnicity_greater_than(reduced_data,'Black Alone',5)
+        self.assertEqual(expected,actual)
     # test ethnicity_less_than
+    def test_ethnicity_less_than(self):
+        expected = [reduced_data[4],reduced_data[5],reduced_data[6]]
+        actual = hw3.ethnicity_less_than(reduced_data,'Asian Alone',1)
+        self.assertEqual(expected,actual)
+    def test_ethnicity_less_than_2(self):
+        percentage = 10
+        expected = []
+        for i in reduced_data:
+            if i.ethnicities['Black Alone'] <10:
+                expected.append(i)
+        actual = hw3.ethnicity_less_than(reduced_data,'Black Alone',percentage)
+        self.assertEqual(expected,actual)
     # test below_poverty_level_greater_than
+    def test_below_poverty_level_greater_than(self):
+        expected = [reduced_data[1],reduced_data[3],reduced_data[4],reduced_data[5]]
+        actual = hw3.below_poverty_level_greater_than(reduced_data,15)
+        self.assertEqual(expected,actual)
+    def test_below_poverty_level_greater_than_2(self):
+        percentage = 10
+        expected = []
+        for i in reduced_data:
+            if i.income['Persons Below Poverty Level'] < 10:
+                expected.append(i)
+        actual = hw3.ethnicity_less_than(reduced_data, 'Persons Below Poverty Level', percentage)
+        self.assertEqual(expected, actual)
     # test below_poverty_level_less_than
+    def test_below_poverty_level_less_than(self):
+        expected = []
+        actual = hw3.below_poverty_level_less_than(reduced_data,10)
+        self.assertEqual(expected,actual)
+    def test_below_poverty_level_less_than_2(self):
+        percentage = 50
+        expected = []
+        for i in reduced_data:
+            if i.income['Persons Below Poverty Level'] < 10:
+                expected.append(i)
+        actual = hw3.ethnicity_less_than(reduced_data, 'Persons Below Poverty Level', percentage)
+        self.assertEqual(expected, actual)
 
 
 
